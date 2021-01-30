fcf0f6d6ddfb6cdd2fd8592dafd7ed50ac719068
Leonardo de Araujo Duarte <leonardodearaujoduarte@gmail.com>
2021-01-30 18:15:56 -0300

    Add interactive player and timing prototype

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,9 @@ const print = std.debug.print;
 const sdl = @cImport(
     @cInclude("SDL2/SDL.h"),
 );
+const time = @cImport(
+    @cInclude("sys/time.h"),
+);
 
 fn sdl_check_error(code: c_int) void {
     if (code != 0) {
@@ -31,8 +34,25 @@ fn render_grid(renderer: ?*sdl.SDL_Renderer, tiles: [HEIGHT][WIDTH]u64) void {
     }
 }
 
+fn render_player(renderer: ?*sdl.SDL_Renderer, x: isize, y: isize, colour: bool) void {
+    const rect = sdl.SDL_Rect{
+        .x = @intCast(c_int, x),
+        .y = @intCast(c_int, y),
+        .w = TILE_SIZE,
+        .h = TILE_SIZE,
+    };
+
+    if (colour) {
+        sdl_check_error(sdl.SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255));
+    } else {
+        sdl_check_error(sdl.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255));
+    }
+
+    sdl_check_error(sdl.SDL_RenderFillRect(renderer, &rect));
+}
+
 pub fn main() !void {
-    sdl_check_error(sdl.SDL_Init(sdl.SDL_INIT_VIDEO));
+    sdl_check_error(sdl.SDL_Init(sdl.SDL_INIT_VIDEO | sdl.SDL_INIT_EVENTS));
 
     const window: ?*sdl.SDL_Window = sdl.SDL_CreateWindow("zig_sdl2", sdl.SDL_WINDOWPOS_UNDEFINED, sdl.SDL_WINDOWPOS_UNDEFINED, 640, 448, sdl.SDL_WINDOW_SHOWN);
 
@@ -48,9 +68,58 @@ pub fn main() !void {
         [_]u64{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
     };
 
-    sdl_check_error(sdl.SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255));
-    sdl_check_error(sdl.SDL_RenderClear(renderer));
-    render_grid(renderer, tiles);
-    sdl.SDL_RenderPresent(renderer);
-    sdl.SDL_Delay(3000);
+    var quit = false;
+    var start: time.timeval = undefined;
+    var end: time.timeval = undefined;
+
+    var x: isize = 0;
+    var y: isize = 0;
+    var colour: bool = false;
+
+    var acc: u64 = 0;
+
+    while (!quit) {
+        _ = time.gettimeofday(&start, null);
+
+        var event: sdl.SDL_Event = undefined;
+
+        while (sdl.SDL_PollEvent(&event) != 0) {
+            if (event.type == sdl.SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        const keyboard_state: [*c]const u8 = sdl.SDL_GetKeyboardState(null);
+
+        if (keyboard_state[sdl.SDL_SCANCODE_W] != 0) {
+            y -= 2;
+        }
+        if (keyboard_state[sdl.SDL_SCANCODE_S] != 0) {
+            y += 2;
+        }
+        if (keyboard_state[sdl.SDL_SCANCODE_A] != 0) {
+            x -= 2;
+        }
+        if (keyboard_state[sdl.SDL_SCANCODE_D] != 0) {
+            x += 2;
+        }
+
+        sdl_check_error(sdl.SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255));
+        sdl_check_error(sdl.SDL_RenderClear(renderer));
+        render_grid(renderer, tiles);
+        render_player(renderer, x, y, colour);
+        sdl.SDL_RenderPresent(renderer);
+
+        _ = time.gettimeofday(&end, null);
+
+        var delta = (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec;
+
+        acc += @intCast(u64, delta);
+        if (acc > 2_000_000) {
+            colour = !colour;
+            acc -= 2_000_000;
+        }
+    }
+
+    sdl.SDL_Quit();
 }
